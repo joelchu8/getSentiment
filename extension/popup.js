3ba769c4ba9e9e10ac747c05ad78d58b9fed1554
jc11 <joel.chu@metaswitch.com>
2022-09-23 14:23:43 +0100

    initial files for chromium extension

---
+++ extension/popup.js
@@ -0,0 +1,43 @@
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: setPageBackgroundColor,
+    });
+  });
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+});
+}
+
+// Function to download data to a file
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
